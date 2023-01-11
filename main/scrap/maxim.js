88b3479c70796639ad723bab8b6d92a937b13245
nwilmers <nathan.wilmers@gmail.com>
2023-01-11 16:03:44 -0500

    Maxim's old files

--- main/scrap/maxim.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($){
-	var images = ['jason_sunset','long_woods'];
-	$('body').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + '.jpg)'});
-});
